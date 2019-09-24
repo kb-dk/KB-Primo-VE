d84a2d6514099ebecd36a80569d0ef4af87cc672
zahra <azr41432>
2019-09-24 08:37:59 -0700

    Added search tips (help-menu addon)

--- js/main.js
+++ js/main.js
@@ -7,7 +7,11 @@ import {PrmTopBarBeforeConfig} from './navigation-header/navigation-header';
 // Language switcher
 import {PrmUserAreaExpandableAfterConfig} from './language/prmUserAreaExpandableAfter.component';
 
-angular.module('viewCustom', ['angularLoad'])
+// Search tips (help-menu Visual studio addon - https://github.com/bulib/primo-explore-bu/tree/master/packages/help-menu)
+import 'primo-explore-help-menu';
+import {list_of_elements} from './help-menu/custum-help-menu-content';
+
+angular.module('viewCustom', ['angularLoad', 'helpMenuTopbar'])
     .run(['$rootScope', ($rootScope) => {
         let query = window.location.search.substring(1);
         query = query.substring(query.indexOf('vid')+4);
@@ -16,6 +20,8 @@ angular.module('viewCustom', ['angularLoad'])
     }]);
 
 angular.module('viewCustom')
+
+.constant('helpMenuConfig', { "list_of_elements": list_of_elements })
 
 // Footer
 .component(PrmExploreFooterAfterConfig.name, PrmExploreFooterAfterConfig.config)
