ef71ac58a0ad2ab4e84c7d3df942cafd44643615
zahra <azr41432>
2019-09-30 08:43:41 -0700

    Added translation possibility to help menu

--- js/help-menu/list_of_elements_en.js
+++ js/help-menu/list_of_elements_en.js
@@ -1,6 +1,6 @@
 export const list_of_elements_en = [
     {
-        "id":"getting-started",
+        "id":"getting-started-en",
         "title":"Getting Started",
         "description":"brief static html content with an overview on how to use BULS; links to direct to main BULS help page in WordPress, other pages, and/or other sections of the menu",
         "icon":{"code":"description","group":"action"},
@@ -15,79 +15,79 @@ export const list_of_elements_en = [
     `
     },
     {
-        "id":"tutorials",
+        "id":"tutorials-en",
         "title":"Tutorials",
         "description":"at a minimum, links to individual videos and a playlist of existing BULS video tutorials (tbd if in Kaltura, WordPress, or youtube); if possible and desired, embed using Kaltura embed code",
         "icon":{"code":"shop_two","group":"action"},
     },
     {}, // DIVIDER, // search-specific
     {
-        "id":"whats-in-search",
+        "id":"whats-in-search-en",
         "title":"What's in Search?",
         "description":`brief description of material types, "scopes", and collections; may have submenus for each of the above; likely also link out to WordPress page with all PCI collections`,
         "icon":{"code":"toc","group":"action"}
     },
     {
-        "id":"didnt-find",
+        "id":"didnt-find-en",
         "title":"Didn't find it?",
         "description":`TBD info about ILL, scope of collections, and problem cases`,
         "icon":{"code":"swap_horiz","group":"action"},
     },
     {}, //DIVIDER, // conceptual
     {
-        "id":"guides",
+        "id":"guides-en",
         "title":"Guides",
         "description":`info about research, subject, course, and how to guides; how to find in BULS by way of search and related more info links; link out to LibGuides home`,
         "icon":{"code":"directions","group":"maps"}
     },
     {
-        "id":"glossary",
+        "id":"glossary-en",
         "title":"Glossary",
         "description":"list of common terms (jargon) and definitions",
         "icon":{"code":"view_list","group":"action"}
     },
     {}, //DIVIDER,
     {
-        "id":"query-builder",
+        "id":"query-builder-en",
         "title":"Query Builder",
         "description":`placeholder for this, tbd functionality for future development; interim step may just be to have text demonstrating adv query syntax`,
         "icon":{"code":"find_in_page","group":"action"}
     },
     {}, //DIVIDER, Search Help
     {
-        "id":"saving-results",
+        "id":"saving-results-en",
         "title":"Saving Results",
         "description":"information about My Favorites, exporting results, ref managers",
         "icon":{"code":"save","group":"content"}
     },
     {
-        "id":"citing-sources",
+        "id":"citing-sources-en",
         "title":"Citing Sources",
         "description":"likely, but may depend on if including saving results; would have info about ref managers",
         "icon":{"code":"create","group":"content"}
     },
     {
-        "id":"account",
+        "id":"account-en",
         "title":"Account",
         "description":"information about My Library Account functionality- loans, renewals, policies?; may also highlight or link to My Library Account",
         "icon":{"code":"account_box","group":"action"}
     },
     {}, //DIVIDER,
     {
-        "id":"for-instructors",
+        "id":"for-instructors-en",
         "title":"For Instructors",
         "description":`may be more "For" sections; will need considerable content work`,
         "icon":{"code":"info","group":"action"},
     },
     {}, //DIVIDER, communication
     {
-        "id":"ask-us",
+        "id":"ask-us-en",
         "title":"Ask Us",
         "description":"needs definition; how to point to chat? link to ask a librarian?",
         "icon":{"code":"forum","group":"communication"}
     },
     {
-        "id":"feedback",
+        "id":"feedback-en",
         "title":"Feedback",
         "description":`directs to a new form to send feedback in the format of specific questions, e.g. "rate your experience", "did you find what you're looking for?", "send us your thoughts on improving search"; will want to clearly differentiate from Ask and reference help, so that patrons who need help soon can get it`,
         "icon":{"code":"chat","group":"communication"}
