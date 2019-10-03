9e2d66fd535894d2d65414f49c53ec5d637b7f62
knutanton <knab@kb.dk>
2019-10-03 14:42:27 +0200

    dk file updated

--- js/help-menu/list_of_elements_da.js
+++ js/help-menu/list_of_elements_da.js
@@ -2,94 +2,94 @@ export const list_of_elements_da = [
     {
         "id":"getting-started-da",
         "title":"Kom i gang",
-        "description":"brief static html content with an overview on how to use BULS; links to direct to main BULS help page in WordPress, other pages, and/or other sections of the menu",
+        "description":"her er en kort intro-tekst til KBs Discovery, evt med links til mere udførlige søgevejledninger andre steder, eller til øvrige sektioner",
         "icon":{"code":"description","group":"action"},
         "template":`
-      <p>KB Libraries Search contains articles, books, journals, databases, films, music, dissertations, and other scholarly materials for your research</p>
-      <h2>Search Tips</h2>
-      <code>brief info about title or subject keyword searches</code>
+      <p>KB Dsicovery indeholder artiker, bøger, tidsskrifter, databaser, film, musik, afhandlinger og andet mateiriale til dine studier og forskning</p>
+      <h2>Søgetips</h2>
+      <code>evt kort info om søgning; der kan benyttes særlig formattering etc.</code>
       <br /><br />
-      <h2>Filtering</h2>
-      <p>Use the filters to limit to one or more material types (books, articles)</p>
-      <p>Use Peer-Reviewed Articles to quickly limit to only these results...</p>
+      <h2>Filtrering</h2>
+      <p>Brug facetterne i venstre side til at afgrænse dit resultat...</p>
+      <p>Afgræns til e-ressourcer, fysiske materialer, artikler fra Peer-Reviewed tidsskrifter ... </p>
     `
     },
     {
         "id":"tutorials-da",
-        "title":"Tutorials",
-        "description":"at a minimum, links to individual videos and a playlist of existing BULS video tutorials (tbd if in Kaltura, WordPress, or youtube); if possible and desired, embed using Kaltura embed code",
+        "title":"Tutorials - skal vi have det? kan fjernes midlertidigt",
+        "description":"at a minimum, links to individual videos and a playlist of existing KB video tutorials",
         "icon":{"code":"shop_two","group":"action"},
     },
     {}, // DIVIDER, // search-specific
     {
         "id":"whats-in-search-da",
-        "title":"What's in Search?",
-        "description":`brief description of material types, "scopes", and collections; may have submenus for each of the above; likely also link out to WordPress page with all PCI collections`,
+        "title":"Hvad søger jeg i?",
+        "description":`kort beskrivelse af materialetyper og samlinger, evt. også undermenuer. Nok også link ud til oversigt over CDI samlinger`,
         "icon":{"code":"toc","group":"action"}
     },
     {
         "id":"didnt-find-da",
-        "title":"Didn't find it?",
-        "description":`TBD info about ILL, scope of collections, and problem cases`,
+        "title":"Fandt du ikke det du søgte?",
+        "description":`Korte forslag, evt link til opdateret version af <a href='https://kub.kb.dk/c.php?g=579871&p=4002189'>denne side</a> eller lignende`,
         "icon":{"code":"swap_horiz","group":"action"},
     },
     {}, //DIVIDER, // conceptual
     {
         "id":"guides-da",
-        "title":"Guides",
-        "description":`info about research, subject, course, and how to guides; how to find in BULS by way of search and related more info links; link out to LibGuides home`,
+        "title":"Fagsider",
+        "description":`Skal vi have links til de forskellige universitetsbibliotekers fagsider her?`,
         "icon":{"code":"directions","group":"maps"}
     },
     {
         "id":"glossary-da",
-        "title":"Glossary",
-        "description":"list of common terms (jargon) and definitions",
+        "title":"Ordliste - giver det mening",
+        "description":"list of common terms (jargon) and definitions - måske bedre helt at undgå",
         "icon":{"code":"view_list","group":"action"}
     },
     {}, //DIVIDER,
     {
         "id":"query-builder-da",
-        "title":"Query Builder",
+        "title":"Query Builder - foreslår, at vi dropper denne",
         "description":`placeholder for this, tbd functionality for future development; interim step may just be to have text demonstrating adv query syntax`,
         "icon":{"code":"find_in_page","group":"action"}
     },
     {}, //DIVIDER, Search Help
     {
         "id":"saving-results-da",
-        "title":"Saving Results",
-        "description":"information about My Favorites, exporting results, ref managers",
+        "title":"Favoritlister og gemte søgninger",
+        "description":"information om Mine Favoritter, og hvad vi kan gøre dem, gemte søgninger, søgeagenter",
         "icon":{"code":"save","group":"content"}
     },
     {
         "id":"citing-sources-da",
-        "title":"Citing Sources",
-        "description":"likely, but may depend on if including saving results; would have info about ref managers",
+        "title":"Styr på kilderne",
+        "description":"kort oversigt over Primos muligheder, med links til flere informationer om ref managers",
         "icon":{"code":"create","group":"content"}
     },
     {
         "id":"account-da",
-        "title":"Account",
-        "description":"information about My Library Account functionality- loans, renewals, policies?; may also highlight or link to My Library Account",
+        "title":"Min konto",
+        "description":"information om min konto  - reserverng, udlån, fornyelser, gebyrer evt link til kontoen",
         "icon":{"code":"account_box","group":"action"}
     },
     {}, //DIVIDER,
     {
         "id":"for-instructors-da",
-        "title":"For Instructors",
+        "title":"For Undervisere - ? ",
         "description":`may be more "For" sections; will need considerable content work`,
         "icon":{"code":"info","group":"action"},
     },
     {}, //DIVIDER, communication
     {
         "id":"ask-us-da",
-        "title":"Ask Us",
+        "title":"Spørg biblioteket",
         "description":"needs definition; how to point to chat? link to ask a librarian?",
         "icon":{"code":"forum","group":"communication"}
     },
     {
         "id":"feedback-da",
-        "title":"Feedback",
-        "description":`directs to a new form to send feedback in the format of specific questions, e.g. "rate your experience", "did you find what you're looking for?", "send us your thoughts on improving search"; will want to clearly differentiate from Ask and reference help, so that patrons who need help soon can get it`,
+        "title":"Feedback - behøver vi det?",
+        "description":`ved ikke om vi ønsker at linke til en særlige formular for dette`,
         "icon":{"code":"chat","group":"communication"}
     }
 ];
