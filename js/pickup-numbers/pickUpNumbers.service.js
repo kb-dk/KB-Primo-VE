a6a7318b5dda8376e00ac8920dc405aff61a3b72
zahra <azr41432>
2020-04-14 06:08:47 -0700

    Fixed the bug with pickup numbers were shown only the first time

--- js/pickup-numbers/pickUpNumbers.service.js
+++ js/pickup-numbers/pickUpNumbers.service.js
@@ -4,11 +4,11 @@
 export class PickUpNumbersService {
     constructor($http, $location) {
         this.$http = $http;
+        this.location = $location;
         this._serviceBaseUrl = 'https://developer.statsbiblioteket.dk/alma-pickupnumbers/services/public/pickupnumber/';
         this._pickUpNumbersForIds = {};
-        this._ongoingInsertions = 0;
         this._runningPromise;
-        this.location = $location;
+        this._ongoingInsertions = 0;
     }
 
     /**
@@ -22,7 +22,7 @@ export class PickUpNumbersService {
         ctrl.$scope.$watch(() => ctrl.selector(ctrl.parentElement).length,
             (newVal, oldVal) => {
                 if (newVal && newVal !== oldVal) {
-                    this.insertPickUpNumbers(ctrl.parentElement, ctrl.parentCtrl.requestsService.requestsDisplay, ctrl.selector);
+                    this.retrieveAndInsertPickUpNumbers(ctrl.parentCtrl, ctrl.parentElement, ctrl.parentCtrl.requestsService.requestsDisplay, ctrl.selector);
                 }
             }
         );
@@ -45,48 +45,71 @@ export class PickUpNumbersService {
      *  @return {Promise} A Promise to be resolved
      *    when the pick-up numbers are inserted.
      */
-    insertPickUpNumbers(targetContainer, requests, selector) {
+    retrieveAndInsertPickUpNumbers(parentCtrl, targetContainer, requests, selector) {
         if (this._runningPromise) {
+
             // If there is an ongoing insertion,
             // perform the new insertion when the former is done.
             return this._runningPromise.then(() => {
-                return this._insert(targetContainer, requests, selector);
+                return this._insert(parentCtrl, targetContainer, requests, selector);
             });
         } else {
             // Else, perform the insertion.
-            return this._insert(targetContainer, requests, selector);
+            return this._insert(parentCtrl, targetContainer, requests, selector);
         }
     }
 
-    _insert(targetContainer, requests, selector) {
+    getPickupLabel(){
+        let lang = this.location.search().lang;
+        let pickUpLabel;
+        if (lang === 'en') {
+            pickUpLabel = 'Pickup number: ';
+        } else {
+            pickUpLabel = 'Ventehyde nummer: ';
+        }
+        return pickUpLabel;
+    }
+    insertPickUpNumber(pickUpNumber, request){
+        if (request.element.innerHTML.indexOf('<span class="pickupnumber">') === -1){
+            let pickUpLabel = this.getPickupLabel();
+            let newElement = "<br/><span class='pickupnumber'>" + pickUpLabel + "</span>" + pickUpNumber + "<br/>";
+            angular.element(request.element).append(newElement);
+        }
+    }
+
+    _insert(parentCtrl, targetContainer, requests, selector) {
         let ctrl = this;
         ctrl._runningPromise = new Promise((resolve, reject) => {
-
             let targetElements = selector(targetContainer);
             let requestObjects = ctrl._composeRequestObjects(targetElements, requests);
-
             ctrl._ongoingInsertions = requestObjects.length;
 
-            requestObjects.forEach((request) => {
-
-                ctrl._insertForRequest(request).then(() => {
+            // Making one promise per request
+            let inserts = [];
+            requestObjects.forEach((request, key) => {
+                inserts[key] = ctrl._insertForRequest(parentCtrl.requestsDisplay[key], request);
+                inserts[key]
+                // Instead of using both then and catch methods and counting down to see when all the promises settled
+                // one can use Promise.allSettled
+                // But it is not supported in ie and some other browsers yet
+                    .then(()=>{
                     ctrl._ongoingInsertions = ctrl._ongoingInsertions - 1;
-                    if (ctrl._ongoingInsertions === 0) {
-                        resolve();
-                        return;
-                    }
-                });
+                        if (ctrl._ongoingInsertions === 0) {
+                            resolve();
+                            return;
+                        }
+                    })
+                    .catch(()=>{
+                        ctrl._ongoingInsertions = ctrl._ongoingInsertions - 1;
+                        if (ctrl._ongoingInsertions === 0) {
+                            resolve();
+                            return;
+                        }
+                    })
             });
-
-        });
-
-        // TODO: This looks weird, but seems to work OK.
-        this._runningPromise.then(() => {
-            this._runningPromise = null;
         });
 
         return this._runningPromise;
-
     }
 
     // Returns an array of objects with request related data.
@@ -101,46 +124,39 @@ export class PickUpNumbersService {
     };
 
     // Inserts the pickup number for given request.
-    _insertForRequest(request) {
+    _insertForRequest(requestsDisplay, request) {
         let ctrl = this;
-
         return new Promise((resolve, reject) => {
             // If there is no DOM element to be altered,
             // do nothing.
             if (!request.element) {
-                resolve();
+                reject();
                 return;
             }
             if (ctrl._pickUpNumbersForIds[request.id]) {
                 // If the pick-up number for the request is already known, use it.
-                ctrl._replaceIdText(request, ctrl._pickUpNumbersForIds[request.id]);
+                // ctrl._replaceIdText(request, ctrl._pickUpNumbersForIds[request.id]);
+                this.insertPickUpNumber(ctrl._pickUpNumbersForIds[request.id], request);
                 resolve();
             } else if (request.expandedDisplay.find((field) => field.label === "request.holds.request_date")) {
                 // Else, if it has a hold deadline, retrieve and insert the pick-up number.
                 // The requested item can only have a pick up number if it has a hold deadline.
-                ctrl._retrievePickUpNumber(request.id).then((response) => {
-                    // Insert the pick-up number text.
-                    let pickUpNumber = response.data.number;
-                    ctrl._pickUpNumbersForIds[request.id] = pickUpNumber;
-                    let lang = this.location.search().lang;
-                    let pickUpLabel;
-                    if (lang === 'en'){
-                       pickUpLabel = 'Pickup number:';
-                    }else{
-                        pickUpLabel = 'Ventehyde nummer::';
-                    }
-                    let newElement = "<br/><span>"+ pickUpLabel +"</span>"+ pickUpNumber +"<br/>";
-                    angular.element(request.element).append(newElement);
-                    resolve();
-                }).catch(() => {
-                    ctrl._removeIdText(request);
-                    console.log('KB: Could not retrieve the pick up number.');
-                    resolve();
-                });
+                ctrl._retrievePickUpNumber(request.id)
+                    .then(response => {
+                        // Insert the pick-up number text.
+                        let pickUpNumber = response.data.number;
+                        ctrl._pickUpNumbersForIds[request.id] = pickUpNumber;
+                        this.insertPickUpNumber(pickUpNumber, request);
+                        resolve();
+                    })
+                    .catch(err => {
+                        // ctrl._removeIdText(request);
+                        console.log('Could not retrieve the pick up number.');
+                        reject(err);
+                        return err;
+                    });
 
             } else {
-                // Else, remove the request ID from the view.
-                ctrl._removeIdText(request);
                 resolve();
             }
 
@@ -148,24 +164,12 @@ export class PickUpNumbersService {
 
     }
 
-    // Removes the request ID.
-    _removeIdText(request) {
-        this._replaceIdText(request, "");
-    };
-
     // Retrieves the pick up numbers from the associated service.
     // Request URL may look like the following.
     _retrievePickUpNumber(requestId) {
-        let serviceURL = this._serviceBaseUrl;
-        serviceURL = serviceURL + requestId;
-        return this.$http.get(serviceURL);
+        return this.$http.get(this._serviceBaseUrl + requestId);
     }
 
-
-    // Replaces the request ID text with the given string.
-    _replaceIdText(request, text) {
-        request.element.textContent = request.element.textContent.replace("-" + request.id, text);
-    };
 
 }
 
