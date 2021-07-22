eee681c79274ec872c17b68ea6d6001387d7547d
zahra <zahramousavidk@gmail.com>
2021-07-22 00:16:43 -0700

    Got translations from API and transferred them to a json

--- js/footer/kbHelp.component.js
+++ js/footer/kbHelp.component.js
@@ -14,22 +14,42 @@ class KbHelpController {
         this.helpMenuHeaderTemplateUrl = 'custom/' + viewName + '/html/help/help-menu-header.html';
         this.helpMenuBodyTemplateUrl = 'custom/' + viewName + '/html/help/help-menu-body.html';
         this.helpSubMenuBodyTemplateUrl = 'custom/' + viewName + '/html/help/help-sub-menu-body.html';
-        // this.start();
+        this.help = {};
     };
 
-    start(){
+    $onInit() {
+        this.getHelpTextFromAPI();
+    }
+
+    getHelpTextFromAPI(){
         this._getTexts()
             .then(response => {
-                console.log('label:', response.data);
-                resolve();
+                this.help = this.createHelpJson(response.data);
+                console.log(this.help);
             })
             .catch(err => {
-                // this._removeIdText(request);
-                console.log('Could not retrieve the pick up number.');
-                reject(err);
+                console.log( err);
                 return err;
             });
     }
+
+    createHelpJson(translationObject) {
+        let help = {};
+        let helpKeys = Object.keys(translationObject).filter(v => v.startsWith('fulldisplay.help'));
+        help.options = [];
+        help.mainHeader = translationObject["fulldisplay.help.mainheader"];
+
+        for (let i = 0; i < Math.floor((helpKeys.length - 1) / 4); i++) {
+            help.options.push({
+                subheader: translationObject[`fulldisplay.help.subheader${i + 1}`],
+                submenu: translationObject[`fulldisplay.help.sub${i + 1}`],
+                iconset: translationObject[`fulldisplay.help.iconset${i + 1}`],
+                icondefinition: translationObject[`fulldisplay.help.icondefinition${i + 1}`],
+            });
+        }
+        return help;
+    }
+
 
     _getTexts(){
         return this.$http.get(`https://api-eu.hosted.exlibrisgroup.com/primo/v1/translations/45KBDK_KGL:KGL?lang=da&apikey=l8xx9dad8184bb0544f48c23c442bcfc0d3e`);
