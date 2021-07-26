9f55cc85f93caac6cd7befd5c42f92dd26682571
zahra <zahramousavidk@gmail.com>
2021-07-26 01:14:57 -0700

    Cleaned up in the component

--- js/footer/kbHelp.component.js
+++ js/footer/kbHelp.component.js
@@ -2,13 +2,11 @@
 class KbHelpController {
 
     constructor($http, $mdDialog, $location, viewName) {
+        this.lang = this.parentCtrl.lang;
         this.$http = $http;
         this.$mdDialog = $mdDialog;
-        this.$location = $location;
+        this.lang = $location.search().lang;
         this.bodyContent = 'main-menu';
-        this.menuType = 'mainMenu';
-        this.mainTitle = 'Hjælp til søgning';
-        this.subTitle = 'sub title';
         this.helpButtonTemplateUrl = 'custom/' + viewName + '/html/help/help-button.html';
         this.helpMenuTemplateUrl = 'custom/' + viewName + '/html/help/help-menu.html';
         this.helpMenuHeaderTemplateUrl = 'custom/' + viewName + '/html/help/help-menu-header.html';
@@ -18,11 +16,12 @@ class KbHelpController {
     };
 
     $onInit() {
+        console.log(this.lang);
         this.getHelpTextFromAPI();
     }
 
     getHelpTextFromAPI(){
-        this._getTexts()
+        this._getTranslations()
             .then(response => {
                 this.help = this.createHelpJson(response.data);
                 console.log(this.help);
@@ -35,24 +34,30 @@ class KbHelpController {
 
     createHelpJson(translationObject) {
         let help = {};
-        let helpKeys = Object.keys(translationObject).filter(v => v.startsWith('fulldisplay.help'));
+        let subheaders = Object.keys(translationObject).filter(v => v.startsWith('fulldisplay.help.subheader'));
         help.options = [];
         help.mainHeader = translationObject["fulldisplay.help.mainheader"];
 
-        for (let i = 0; i < Math.floor((helpKeys.length - 1) / 4); i++) {
-            help.options.push({
-                subheader: translationObject[`fulldisplay.help.subheader${i + 1}`],
-                submenu: translationObject[`fulldisplay.help.sub${i + 1}`],
-                iconset: translationObject[`fulldisplay.help.iconset${i + 1}`],
-                icondefinition: translationObject[`fulldisplay.help.icondefinition${i + 1}`],
-            });
+        for (let i = 0; i < subheaders.length; i++) {
+            if (translationObject[`fulldisplay.help.subheader${i + 1}`] === 'line'){
+                help.options.push({
+                    subheader: 'line',
+                });
+            }else {
+                help.options.push({
+                    subheader: translationObject[`fulldisplay.help.subheader${i + 1}`],
+                    submenu: translationObject[`fulldisplay.help.sub${i + 1}`],
+                    iconset: translationObject[`fulldisplay.help.iconset${i + 1}`],
+                    icondefinition: translationObject[`fulldisplay.help.icondefinition${i + 1}`],
+                });
+            }
         }
         return help;
     }
 
 
-    _getTexts(){
-        return this.$http.get(`/primaws/rest/pub/translations/45KBDK_KGL:KGL?lang=da`);
+    _getTranslations(){
+        return this.$http.get(`/primaws/rest/pub/translations/45KBDK_KGL:KGL?lang=${this.lang}`);
     }
 
     openHelpMenu() {
