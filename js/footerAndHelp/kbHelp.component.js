15cd1fd62451767ade3b57d790ee28e5497e4f74
zahra <zahramousavidk@gmail.com>
2021-08-11 06:03:48 -0700

    Fixed mixed up with Primo's md-dialog

--- js/footerAndHelp/kbHelp.component.js
+++ js/footerAndHelp/kbHelp.component.js
@@ -1,10 +1,10 @@
 
 class KbHelpController {
 
-    constructor($http, $mdDialog, $location, viewName) {
+    constructor($http, $location, $mdDialog, viewName) {
         this.$http = $http;
         this.$mdDialog = $mdDialog;
-        this.lang = $location.search().lang || "da";
+        this.lang = "da";
         this.bodyContent = 'main-menu';
         this.helpButtonTemplateUrl = 'custom/' + viewName + '/html/help/help-button.html';
         this.helpMenuTemplateUrl = 'custom/' + viewName + '/html/help/help-menu.html';
@@ -14,82 +14,87 @@ class KbHelpController {
         this.help = {};
     };
 
-    $onInit() {
-        console.log('lang:',this.lang);
-        this.getHelpTextFromAPI();
-    }
-
-    getHelpTextFromAPI(){
-        this._getTranslations()
-            .then(response => {
-                this.help = this.createHelpJson(response.data);
-            })
-            .catch(err => {
-                console.log( err);
-                return err;
-            });
-    }
-
-    createHelpJson(translationObject) {
-        let help = {};
-        let subheaders = Object.keys(translationObject).filter(v => v.startsWith('fulldisplay.help.subheader'));
-        help.options = [];
-        help.mainHeader = translationObject["fulldisplay.help.mainheader"];
-        let subheadersLength =  subheaders ? subheaders.length : 0;
-
-        for (let i = 0; i < subheadersLength; i++) {
-            if (translationObject[`fulldisplay.help.subheader${i + 1}`] === 'line'){
-                help.options.push({
-                    subheader: 'line',
-                });
-            }else {
-                help.options.push({
-                    subheader: translationObject[`fulldisplay.help.subheader${i + 1}`],
-                    submenu: translationObject[`fulldisplay.help.sub${i + 1}`],
-                    iconset: translationObject[`fulldisplay.help.iconset${i + 1}`],
-                    icondefinition: translationObject[`fulldisplay.help.icondefinition${i + 1}`],
-                });
-            }
+        $onInit() {
+            console.log('lang:',this.lang);
+            this.getHelpTextFromAPI();
         }
-        return help;
-    }
+
+        getHelpTextFromAPI(){
+            this._getTranslations()
+                .then(response => {
+                    this.help = this.createHelpJson(response.data);
+                })
+                .catch(err => {
+                    console.log( err);
+                    return err;
+                });
+        }
+
+        createHelpJson(translationObject) {
+            let help = {};
+            let subheaders = Object.keys(translationObject).filter(v => v.startsWith('fulldisplay.help.subheader'));
+            help.options = [];
+            help.mainHeader = translationObject["fulldisplay.help.mainheader"];
+            let subheadersLength =  subheaders ? subheaders.length : 0;
+
+            for (let i = 0; i < subheadersLength; i++) {
+                if (translationObject[`fulldisplay.help.subheader${i + 1}`] === 'line'){
+                    help.options.push({
+                        subheader: 'line',
+                    });
+                }else {
+                    help.options.push({
+                        subheader: translationObject[`fulldisplay.help.subheader${i + 1}`],
+                        submenu: translationObject[`fulldisplay.help.sub${i + 1}`],
+                        iconset: translationObject[`fulldisplay.help.iconset${i + 1}`],
+                        icondefinition: translationObject[`fulldisplay.help.icondefinition${i + 1}`],
+                    });
+                }
+            }
+            return help;
+        }
 
 
-    _getTranslations(){
-        return this.$http.get(`/primaws/rest/pub/translations/45KBDK_KGL:KGL?lang=${this.lang}`);
-    }
+        _getTranslations(){
+            return this.$http.get(`/primaws/rest/pub/translations/45KBDK_KGL:KGL?lang=${this.lang}`);
+        }
 
     openHelpMenu() {
         this.bodyContent = 'main-menu';
-        this.$mdDialog.show({
-            contentElement: '#helpMenu',
-            parent: angular.element(document.body),
-            hasBackdrop: true,
-            multiple: false,
-            clickOutsideToClose:true,
-            fullscreen: false,
-            focusOnOpen: true
-        });
+        let helpMenu = document.getElementById('helpMenu');
+        // Remove and add md-dialog-container to the helpMenu so it doesn't interfere with Primo's
+        if (!angular.element(document.querySelectorAll('#helpMenu')).hasClass('md-dialog-container')) {
+            helpMenu.classList.add('md-dialog-container');
+            this.$mdDialog.show({
+                contentElement: '#helpMenu',
+                parent: angular.element(document.getElementsByName('kb-help')[0]),
+                // Not using standard backdrop because it is interfering with Primo's
+                hasBackdrop: false,
+                multiple: true,
+                clickOutsideToClose: false,
+                fullscreen: true,
+                focusOnOpen: true,
+            });
+        }
     };
 
-    hideHelpMenu(){
-        this.$mdDialog.hide();
-    };
+        hideHelpMenu(){
+            this.$mdDialog.cancel();
+            let helpMenu = document.getElementById('helpMenu');
+            helpMenu.classList.remove('md-dialog-container');
+        };
 
-    changeBodyContent(id){
-        this.bodyContent = id;
-    };
+        changeBodyContent(id){
+            this.bodyContent = id;
+        };
 
 }
 
-KbHelpController.$inject = ['$http', '$mdDialog', '$location', 'viewName'];
+KbHelpController.$inject = ['$http', '$location', '$mdDialog', 'viewName'];
 
 export let KbHelpConfig = {
     name: 'kbHelp',
     config: {
-        bindings: {
-            parentCtrl: '<'
-        },
         controller: KbHelpController,
         templateUrl: ["viewName", function(viewName){
             return 'custom/' + viewName + '/html/help/help.html';
