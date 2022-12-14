79b3bc00c9f49ba5e647c18c72ce3e95acfa008f
zahramousavi <zahramousavidk@gmail.com>
2022-12-14 15:42:54 +0100

    Added close button to 'report broken link' header.

--- js/brokenLinkFeedback/prmReportProblemAfter.js
+++ js/brokenLinkFeedback/prmReportProblemAfter.js
@@ -1,11 +1,13 @@
 class PrmReportProblemAfterController {
-    constructor() {
-    }
     $onInit() {
-        let closeReportBrokenLink = document.getElementById('closeReportBrokenLink');
-        let header = angular.element(document.querySelectorAll('prm-report-problem .report-header'));
-        header.append(closeReportBrokenLink);
-        console.log(header, closeReportBrokenLink);
+        let openReportBrokenLink = angular.element(document.querySelectorAll('prm-report-problem .report-problem-btn'))[0];
+        openReportBrokenLink.addEventListener('click', function () {
+            let closeReportBrokenLink = angular.element('<prm-icon svg-icon-set="navigation" icon-definition="ic_close_24px" id="closeReportBrokenLink" onclick="document.querySelectorAll(\'prm-report-problem .report-problem-btn\')[0].click();" id="closeReportBrokenLink"><svg width="100%" height="100%" viewBox="0 0 24 24" id="ic_close_24px_cache28" y="240" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg></prm-icon>');
+            let header = angular.element(document.querySelectorAll('prm-report-problem .report-header'));
+            if(header && closeReportBrokenLink){
+                header.append(closeReportBrokenLink);
+            }
+        });
     }
 }
 
@@ -13,6 +15,5 @@ export let PrmReportProblemAfterConfig = {
     name: 'prmReportProblemAfter',
     config: {
         controller: PrmReportProblemAfterController,
-        template:`<div id="closeReportBrokenLink">X</div>`,
     }
 };
