a5004cda2a5928611f8fd65495fa3e943a8e76cc
zahramousavi <zahramousavidk@gmail.com>
2023-06-12 11:21:04 +0200

    Added new pickup numbers.

--- js/pickup-numbers/pickUpNumbers.service.js
+++ js/pickup-numbers/pickUpNumbers.service.js
@@ -1,61 +1,9 @@
 /**
- * Service to insert the pick up numbers for the requested items.
+ * Service to insert the pickup numbers for the requested items.
  */
 export class PickUpNumbersService {
-    constructor($http, $location) {
-        this.$http = $http;
+    constructor($location) {
         this.location = $location;
-        this._pickUpNumbersForIds = {};
-        this._runningPromise;
-        this._ongoingInsertions = 0;
-    }
-
-    /**
-     * Binds a watcher to the given controller.
-     * The watcher watches for the elements
-     * containing the request IDs, and when the
-     * elements become available, inserts the pickup
-     * numbers if they exist.*/
-
-    waitForIdsAndInsertPickUpNumbers(ctrl) {
-        ctrl.$scope.$watch(() => ctrl.selector(ctrl.parentElement).length,
-            (newVal, oldVal) => {
-                if (newVal && newVal !== oldVal) {
-                    this.retrieveAndInsertPickUpNumbers(ctrl.parentCtrl, ctrl.parentElement, ctrl.parentCtrl.requestsService.requestsDisplay, ctrl.selector);
-                }
-            }
-        );
-    }
-
-    /**
-     *  Method to retrieve and insert the pick-up numbers
-     *  into the given DOM element.
-     *  If the method is called when a previous run
-     *  has not finished, it chains the new insertion
-     *  into the promise of the previous call.
-     *  @param {Object} targetContainer - A DOM element
-     *    containing the elements the pick-up
-     *    numbers are to be inserted.
-     *  @param {Array} requests - An array of request items,
-     *    pick-up numbers of which are to be retrieved.
-     *  @param {function} selector - A selector function to return
-     *    the target DOM elements when called with a
-     *    ancestor DOM element.
-     *  @return {Promise} A Promise to be resolved
-     *    when the pick-up numbers are inserted.
-     */
-    retrieveAndInsertPickUpNumbers(parentCtrl, targetContainer, requests, selector) {
-        if (this._runningPromise) {
-
-            // If there is an ongoing insertion,
-            // perform the new insertion when the former is done.
-            return this._runningPromise.then(() => {
-                return this._insert(parentCtrl, targetContainer, requests, selector);
-            });
-        } else {
-            // Else, perform the insertion.
-            return this._insert(parentCtrl, targetContainer, requests, selector);
-        }
     }
 
     getPickupLabel(){
@@ -69,145 +17,12 @@ export class PickUpNumbersService {
         return pickUpLabel;
     }
 
-    getServiceBaseUrl(){
-        let baseUrl = this.location.$$absUrl;
-        if (baseUrl.includes('kbdk-kgl-psb.primo.exlibrisgroup')){
-            return 'https://public-stage.statsbiblioteket.dk/alma-pickupnumbers/services/public/pickupnumber/';
-        } else if (baseUrl.includes('soeg.kb.dk')){
-            return 'https://webservice.kb.dk/alma-pickupnumbers/services/public/pickupnumber/'
-        } else {
-            return 'https://developer.statsbiblioteket.dk/alma-pickupnumbers/services/public/pickupnumber/';
-        }
-    }
-
     insertPickUpNumber(pickUpNumber, container) {
-        if (container.innerHTML.indexOf('<span class="pickupnumber">') === -1) {
-            let newElement = "<br/><span class='pickupnumber'>" + this.getPickupLabel() + "</span>" + pickUpNumber + "<br/>";
+        if (container.innerHTML.indexOf('<span class="pickup_number">') === -1) {
+            let newElement = "<br/><span class='pickup_number'>" + this.getPickupLabel() + "</span>" + pickUpNumber + "<br/>";
             angular.element(container.querySelectorAll('p.request-location')).append(newElement);
         }
     }
-
-    insertPickupnumbersToOverview(requests){
-        let element = document.querySelectorAll('prm-requests-overview');
-        if (element.length>0){
-            this.addPickupnumbersToOverview(element, requests);
-        }else{
-            ctrl.$scope.$watch(() => element.length,
-                (newVal, oldVal) => {
-                    if (element.length) {
-                        this.addPickupnumbersToOverview(element, requests);
-                    }
-                }
-            );
-        }
-
-    };
-
-    addPickupnumbersToOverview(element, requests){
-        let overview_requests = angular.element(document.querySelectorAll('prm-requests-overview div[class="md-list-item-text layout-fill"]'));
-        for (let index = 0; index < Object.keys(overview_requests).length-1; index++) {
-            if((this._pickUpNumbersForIds[requests[index].requestId])&&(overview_requests[index].innerHTML.indexOf('<span class="pickupnumber">') === -1)){
-                let newElement = '<p class="weak-text"><span class="pickupnumber">'+ this.getPickupLabel() +'</span><span>'+this._pickUpNumbersForIds[requests[index].requestId]+'</span></p>';
-                angular.element(overview_requests[index]).append(newElement);
-            }
-        }
 }
 
-    _insert(parentCtrl, targetContainer, requests, selector) {
-        let ctrl = this;
-        ctrl._runningPromise = new Promise((resolve, reject) => {
-            let targetElements = selector(targetContainer);
-            let requestObjects = ctrl._composeRequestObjects(targetElements, requests);
-            ctrl._ongoingInsertions = requestObjects.length;
-
-            // Making one promise per request
-            let inserts = [];
-            requestObjects.forEach((request, key) => {
-                inserts[key] = ctrl._insertForRequest(parentCtrl.requestsDisplay[key], request);
-                inserts[key]
-                // Instead of using both then and catch methods and counting down to see when all the promises settled
-                // one can use Promise.allSettled
-                // But it is not supported in ie and some other browsers yet
-                    .then(()=>{
-                    ctrl._ongoingInsertions = ctrl._ongoingInsertions - 1;
-                        if (ctrl._ongoingInsertions === 0) {
-                            this.insertPickupnumbersToOverview(requests);
-                            resolve();
-                        }
-                    })
-                    .catch(()=>{
-                        ctrl._ongoingInsertions = ctrl._ongoingInsertions - 1;
-                        if (ctrl._ongoingInsertions === 0) {
-                            this.insertPickupnumbersToOverview(requests);
-                            resolve();
-                        }
-                    })
-            });
-        });
-
-        return this._runningPromise;
-    }
-
-    // Returns an array of objects with request related data.
-    _composeRequestObjects(targetElements, requests) {
-        return requests.map((request, index) => {
-            return {
-                element: targetElements[index],
-                id: request.requestId,
-                expandedDisplay: request.expandedDisplay,
-                isAlert: request.isAlert  // isAlert is a Primo field, that most likely says if a booking is ready for pickup or not
-            }
-        });
-    };
-
-    // Inserts the pickup number for given request.
-    _insertForRequest(requestsDisplay, request) {
-        let ctrl = this;
-        return new Promise((resolve, reject) => {
-            // If there is no DOM element to be altered,
-            // do nothing.
-            if (!request.element) {
-                reject();
-                return;
-            }
-            if (ctrl._pickUpNumbersForIds[request.id]) {
-                // If the pick-up number for the request is already known, use it.
-                // ctrl._replaceIdText(request, ctrl._pickUpNumbersForIds[request.id]);
-                this.insertPickUpNumber(ctrl._pickUpNumbersForIds[request.id], request.element);
-                resolve();
-            } else if (request.isAlert) {
-                // Else, if isAlert set to true, retrieve and insert the pick-up number.
-                // The requested item can probably only have a pick up number if its isAlert is set to true.
-                    ctrl._retrievePickUpNumber(request.id)
-                        .then(response => {
-                            // Insert the pick-up number text.
-                            let pickUpNumber = response.data.number;
-                            ctrl._pickUpNumbersForIds[request.id] = pickUpNumber;
-                            this.insertPickUpNumber(pickUpNumber, request.element);
-                            resolve();
-                        })
-                        .catch(err => {
-                            // ctrl._removeIdText(request);
-                            console.log('Could not retrieve the pick up number.');
-                            reject(err);
-                            return err;
-                        });
-
-            } else {
-                resolve();
-            }
-
-        });
-
-    }
-
-    // Retrieves the pick up numbers from the associated service.
-    // Request URL may look like the following.
-    _retrievePickUpNumber(requestId) {
-        return this.$http.get(this.getServiceBaseUrl() + requestId);
-    }
-
-
-}
-
-PickUpNumbersService.$inject = ['$http', '$location'];
+PickUpNumbersService.$inject = ['$location'];
--- js/pickup-numbers/prmRequestsAfter.component.js
+++ js/pickup-numbers/prmRequestsAfter.component.js
@@ -1,23 +1,76 @@
+// https://sbprojects.statsbiblioteket.dk/jira/browse/BSV-437
 class PrmRequestsAfterController {
 
-    constructor($scope, $element, pickUpNumbersService) {
-        this.$scope = $scope;
+    constructor($element, pickUpNumbersService) {
         this.$element = $element;
         this.pickUpNumbersService = pickUpNumbersService;
     }
 
     $onInit() {
-        this.parentElement = this.$element.parent()[0];
-        this.pickUpNumbersService.waitForIdsAndInsertPickUpNumbers(this);
+        setTimeout(this.createAndInsertPickupNumbers, 2000, this);
     }
 
-    selector(element) {
-        return element.querySelectorAll('md-list-item');
+    createAndInsertPickupNumbers(_this) {
+        let reservations = _this.$element.parent()[0].querySelectorAll('md-list-item');
+        for (let requestNumber = 0; requestNumber < reservations.length; requestNumber++) {
+            _this.createAndInsertPickupNumber(requestNumber, reservations);
+        }
     }
 
+    createAndInsertPickupNumber(requestNumber, reservations) {
+        let message = this.getFirstLine(this.parentCtrl, requestNumber);
+        let day = this.getDay(message);
+        if (this.isDay(day)) {
+            let shelf_nr = this.getPickupNumber(requestNumber, day);
+            this.pickUpNumbersService.insertPickUpNumber(shelf_nr, reservations[requestNumber]);
+        }
+    }
+
+    getPickupNumber(requestNumber, day) {
+        let requestId = this.getRequestId(requestNumber);
+        let fourDigits = this.getFourDigits(requestId);
+        return this.getShelfNr(day, fourDigits);
+    }
+
+    // Shelf number is a combination of day from the first line and the 4 digits from the the
+    // '13', '3573' => '13/3573'
+    getShelfNr(day, fourDigits) {
+        return `${day}/${fourDigits} `;
+    }
+
+    // Returns 7. to 11. characters from the end
+    // '42730335730005763' => '3573'
+    getFourDigits(requestId) {
+        return requestId.substring(requestId.length - 11, requestId.length - 7);
+    }
+
+// Example:
+    // => '42730335730005763'
+    getRequestId (requestNumber) {
+        return this.parentCtrl.requestsDisplay[requestNumber].requestId;
+    }
+
+    // Take out the characters nr. 9 and 10 from the end of the message
+    // Examples for the message:
+    // 1: REQUEST. On Hold Shelf until 13/06/2023 => '13'
+    // 2: REQUEST. Not Started. Your place in queue is: 1 => 'n '
+    getDay (message) {
+        return message.substring(message.length - 8, message.length - 10);
+    }
+
+    // Examples for the results:
+    // 1: => 'REQUEST. On Hold Shelf until 13/06/2023'
+    // 2: => 'REQUEST. Not Started. Your place in queue is: 1'
+    getFirstLine(parent, requestNumber) {
+        return parent.requestsDisplay[requestNumber].firstLineRight;
+    }
+
+    isDay(day) {
+        return parseInt(day) >= 1 && parseInt(day) <= 31;
+    }
 }
 
-PrmRequestsAfterController.$inject = ['$scope', '$element', 'pickUpNumbersService'];
+PrmRequestsAfterController.$inject = ['$element', 'pickUpNumbersService'];
 
 export let PrmRequestsAfterConfig = {
     name: 'prmRequestsAfter',
